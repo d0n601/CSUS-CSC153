6224fe85743e645605c7fe242c97eb8fba76bf96
x <me@ryankozak.com>
2019-11-13 11:11:22 -0800

    Starting to add stuff from the project.

---
+++ Project/ddKillDisk.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Ask the user for their name
+
+echo ======================================================================
+echo  Welcome to dd Kill Disk, a simple disk destroyer for Linux systems.
+echo ======================================================================
+echo
+echo
+echo Would you like to list the disk on this machine: \(Y/N\)?
+read list_disks
+
+if [ "$list_disks" == "Y" ]  || [ "$list_disks" == "y" ]
+then
+    sudo fdisk -l
+fi
+
+echo
+echo
+echo =====================================================================
+echo      Choose a disk listed above to be destroyed.
+echo =====================================================================
+echo
+echo Enter the disk to destroy \(ex: /dev/sdb\):
+read get_disk
+echo Enter the number of passes \(ex: 10\): 
+read num_passes
+
+
+for (( c = 1; c <= $num_passes; c++ ))
+do
+   echo "Welcome $c times"
+done
+
+echo
+echo
+echo Your disk is very destroyed, have a nice day.
+echo
+
+
